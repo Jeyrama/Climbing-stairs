3a4a1030dca591554aa671b88134d8efa523381a
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-09-18 12:47:35 -0700

    Create Climbing-stairs.js

---
+++ Climbing-stairs.js
@@ -0,0 +1,16 @@
+/*
+A staircase is given with a non-negative cost per each step. 
+Once you pay the cost, you can either climb one or two steps. 
+Create a solution to find the minimum sum of costs to reach the top 
+(finishing the payments including cost[-2] or cost[-1]). 
+You can either start at cost[0] or cost[1].
+
+Examples:
+  climbing_stairs([0, 2, 2, 1]) ➞ 2
+  climbing_stairs([0, 2, 3, 2]) ➞ 3
+  climbing_stairs([10, 15, 20]) ➞ 15
+  climbing_stairs([0, 0, 0, 0, 0, 0]) ➞ 0
+
+Notes:
+  2 <= len(cost) <= 1000
+*/
